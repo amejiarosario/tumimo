4a76c6a69f10cdd2ced58ed5421bd93b6eabb7f4
Adrian Mejia <adriansky@gmail.com>
2012-10-04 20:12:23 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tumimo::Application.config.secret_token = '50fc3de281b870465f485ceb2012f26ba0556544215b7ab57e0affb79c8d6d0ab1f4f3ea5e3e2fb3e8260749f4bb4899aa0841df71013672c7dcececa217c4e3'
